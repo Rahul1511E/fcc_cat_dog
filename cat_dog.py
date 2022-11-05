07c9eafa25c93136735a4f609a1bd22592b7b438
Rahul <rockr3876@gmail.com>
2022-11-05 13:02:15 +0530

    Update cat_dog.py

--- cat_dog.py
+++ cat_dog.py
@@ -1,17 +1,23 @@
-import tensorflow as tf
-
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-
+#1
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy
 
+#2
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D, Input
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+#3
+!apt-get update -qq && apt-get install -y -qq wget unzip
+!rm -rf cats_and_dogs.zip && rm -rf cats_and_dogs
 !wget https://cdn.freecodecamp.org/project-data/cats-and-dogs/cats_and_dogs.zip
+!ls -lah
+!unzip -o cats_and_dogs.zip
 
-!unzip cats_and_dogs.zip
-
+#4
 PATH = 'cats_and_dogs'
 
 train_dir = os.path.join(PATH, 'train')
@@ -22,22 +28,39 @@ total_train = sum([len(files) for r, d, files in os.walk(train_dir)])
 total_val = sum([len(files) for r, d, files in os.walk(validation_dir)])
 total_test = len(os.listdir(test_dir))
 
-# Variables for pre-processing and training.
 batch_size = 128
 epochs = 15
 IMG_HEIGHT = 150
 IMG_WIDTH = 150
 
-# 3
-train_image_generator = None
-validation_image_generator = None
-test_image_generator = None
+#5
+rescale=1/255
 
-train_data_gen = None
-val_data_gen = None
-test_data_gen = None
+train_image_generator = ImageDataGenerator(rescale=rescale)
+validation_image_generator = ImageDataGenerator(rescale=rescale)
+test_image_generator = ImageDataGenerator(rescale=rescale)
 
-# 4
+train_data_gen = train_image_generator.flow_from_directory(
+    batch_size=batch_size,
+    directory=train_dir,
+    target_size=(IMG_HEIGHT, IMG_WIDTH),
+    class_mode='binary')
+
+val_data_gen = validation_image_generator.flow_from_directory(
+    batch_size=batch_size,
+    directory=validation_dir,
+    target_size=(IMG_HEIGHT, IMG_WIDTH),
+    class_mode='binary')
+
+test_data_gen = test_image_generator.flow_from_directory(
+    batch_size=batch_size,
+    classes=["."],
+    directory=test_dir,
+    target_size=(IMG_HEIGHT, IMG_WIDTH),
+    shuffle=False,
+    class_mode="binary")
+
+#6
 def plotImages(images_arr, probabilities = False):
     fig, axes = plt.subplots(len(images_arr), 1, figsize=(5,len(images_arr) * 3))
     if probabilities is False:
@@ -57,35 +80,69 @@ def plotImages(images_arr, probabilities = False):
 sample_training_images, _ = next(train_data_gen)
 plotImages(sample_training_images[:5])
 
-# 5
-train_image_generator = None
+#7
+train_image_generator = ImageDataGenerator(
+    rescale=rescale,
+    horizontal_flip=True,
+    rotation_range=20,
+    zoom_range=0.15,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.15,
+    fill_mode="nearest"
+)
 
-# 6
-train_data_gen = train_image_generator.flow_from_directory(batch_size=batch_size,
-                                                     directory=train_dir,
-                                                     target_size=(IMG_HEIGHT, IMG_WIDTH),
-                                                     class_mode='binary')
+#8
+train_data_gen = train_image_generator.flow_from_directory(
+    batch_size=batch_size,
+    directory=train_dir,
+    target_size=(IMG_HEIGHT, IMG_WIDTH),
+    class_mode='binary')
 
 augmented_images = [train_data_gen[0][0][0] for i in range(5)]
 
 plotImages(augmented_images)
 
-# 7
-model = Sequential()
+#9
+model = Sequential([
+    Input(shape=(IMG_HEIGHT, IMG_WIDTH, 3)),
+    Conv2D(16, 3, padding='same', activation='relu'),
+    MaxPooling2D(),
+    Conv2D(32, 3, padding='same', activation='relu'),
+    MaxPooling2D(),
+    Conv2D(64, 3, padding='same', activation='relu'),
+    MaxPooling2D(),
+    Dropout(0.2),
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dense(2)
+])
 
-
-
-
-
-
-
+model.compile(
+    optimizer='adam',
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=['accuracy'],
+)
 
 model.summary()
 
-# 8
-history = None
+#10
+from math import ceil
+steps_per_epoch=ceil(len(train_data_gen) / batch_size)
+steps_per_epoch
 
-# 9
+#11
+validation_steps = ceil(len(val_data_gen) / batch_size)
+validation_steps
+
+#12
+history = model.fit(
+    train_data_gen,
+    epochs=epochs,
+    validation_data=val_data_gen,
+)
+
+#13
 acc = history.history['accuracy']
 val_acc = history.history['val_accuracy']
 
@@ -108,14 +165,18 @@ plt.legend(loc='upper right')
 plt.title('Training and Validation Loss')
 plt.show()
 
+#14
+predictions = model.predict(test_data_gen)
+probabilities = [1 if a[0]<a[1] else 0 for a in predictions]
 
+#15
+print(len(probabilities))
 
-
-# 11
+#16
 answers =  [1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0,
             1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0,
             1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1,
-            1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 
+            1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1,
             0, 0, 0, 0, 0, 0]
 
 correct = 0
@@ -124,13 +185,15 @@ for probability, answer in zip(probabilities, answers):
   if round(probability) == answer:
     correct +=1
 
-percentage_identified = (correct / len(answers)) * 100
+percentage_identified = (correct / len(answers))
 
-passed_challenge = percentage_identified >= 63
+passed_challenge = percentage_identified > 0.63
 
-print(f"Your model correctly identified {round(percentage_identified, 2)}% of the images of cats and dogs.")
+print( f"Your model correctly identified {round(percentage_identified*100, 2)}% "
+       f"of the images of cats and dogs.")
 
 if passed_challenge:
   print("You passed the challenge!")
 else:
-  print("You haven't passed yet. Your model should identify at least 63% of the images. Keep trying. You will get it!")
+  print("You haven't passed yet. Your model should identify at least 63% "
+        "of the images. Keep trying. You will get it!")
